f9c2e742e0c6b718f0c18ab587d6ebf42f0aac46
George.K <kuzzz.geo@gmail.com>
2026-06-26 16:23:19 +0300

    chore: initial project structure with apps and custom user model

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'users.User'
+
 
 # Application definition
 
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.users',
+    'apps.portfolio',
 ]
 
 MIDDLEWARE = [
@@ -79,6 +83,7 @@ DATABASES = {
     }
 }
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
