23bfda66f33c9a8b497391a306b58cdef510e036
George.K <kuzzz.geo@gmail.com>
2026-06-26 16:48:32 +0300

    feat: add DRF and health chek endpoint

--- config/settings.py
+++ config/settings.py
@@ -39,8 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'apps.users',
     'apps.portfolio',
+    'apps.api',
 ]
 
 MIDDLEWARE = [
